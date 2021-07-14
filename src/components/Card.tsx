c796137677dcc75657d0afe9fb7bd4eeaaf09afa
Monsterhunter0815 <64657959+Monsterhunter0815@users.noreply.github.com>
2021-07-14 22:30:21 +0200

    minor adjustments regarding the comment syntax

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,7 +5,7 @@ import type { Props } from '$util/types';
 //* Card takes in contents and returns the full Card */
 export default function Card({ children, className, ...props }: Props): JSX.Element {
 	return (
-		//* Main div section containing props */
+		/* Main div section containing props */
 		<div {...props} className={clsx('max-w-xl', className)}>
 			{/* Div section containing children */}
 			<div className='bg-white shadow-md rounded-b-xl dark:bg-black'>{children}</div>
